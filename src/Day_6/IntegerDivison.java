a86a7e472c35ecf99da6b28556a1e623ff2bddf7
ashut <ashutoshkumar7404@gmail.com>
2024-08-08 16:42:41 +0530

    first comit

---
+++ src/Day_6/IntegerDivison.java
@@ -0,0 +1,10 @@
+package Day_6;
+
+public class IntegerDivison {
+    public static void main(String[] args) {
+        int thoeoryMarks=356,practicalMarks=45,totalMarks=500;
+        float percentage=((thoeoryMarks + practicalMarks) * 100)/totalMarks;
+        System.out.print("Percentage=" + percentage + "%");
+
+    }
+}
