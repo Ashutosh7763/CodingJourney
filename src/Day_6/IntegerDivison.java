7bfc7247a78d7553ee172cedf1bc3dc23d4ba2b3
ashut <ashutoshkumar7404@gmail.com>
2024-08-08 17:04:48 +0530

    first comit

--- src/Day_6/IntegerDivison.java
+++ src/Day_6/IntegerDivison.java
@@ -1,10 +1,19 @@
 package Day_6;
 
+import java.util.Scanner;
+
 public class IntegerDivison {
     public static void main(String[] args) {
-        int thoeoryMarks=356,practicalMarks=45,totalMarks=500;
+        float thoeoryMarks,practicalMarks,totalMarks;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Theory Marks:");
+        thoeoryMarks=sc.nextFloat();
+        System.out.println("Enter Practical Marks:");
+        practicalMarks=sc.nextFloat();
+        System.out.println("Enter Total Marks");
+        totalMarks=sc.nextFloat();
         float percentage=((thoeoryMarks + practicalMarks) * 100)/totalMarks;
-        System.out.print("Percentage=" + percentage + "%");
-
+        int round = Math.round(percentage);
+        System.out.print("Percentage=" + round + "%");
     }
 }
